31ed8fe50346cd1858e0850e234af489ec8be5ed
Sayyed Aaman <sayyedaaman9@gmail.com>
2026-08-23 19:57:34 +0530

    feat: set up React Router with all 9 routes (Task 3)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,40 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Customer pages
+import Home from "./pages/customer/Home";
+import RestaurantDetails from "./pages/customer/RestaurantDetails";
+import Cart from "./pages/customer/Cart";
+import Checkout from "./pages/customer/Checkout";
+import OrderTracking from "./pages/customer/OrderTracking";
+
+// Admin pages
+import AdminDashboard from "./pages/admin/Dashboard";
+import OrderDetails from "./pages/admin/OrderDetails";
+
+// Delivery pages
+import DeliveryDashboard from "./pages/delivery/Dashboard";
+import ActiveDelivery from "./pages/delivery/ActiveDelivery";
+
 function App() {
   return (
-    <>
-      <h1>Food Delivery</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Customer */}
+        <Route path="/customer" element={<Home />} />
+        <Route path="/customer/restaurant" element={<RestaurantDetails />} />
+        <Route path="/customer/cart" element={<Cart />} />
+        <Route path="/customer/checkout" element={<Checkout />} />
+        <Route path="/customer/order" element={<OrderTracking />} />
+
+        {/* Admin */}
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/order" element={<OrderDetails />} />
+
+        {/* Delivery */}
+        <Route path="/delivery" element={<DeliveryDashboard />} />
+        <Route path="/delivery/active" element={<ActiveDelivery />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
